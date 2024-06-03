59f3e52032f06e1f2743ba97544db50313d67a35
luccavilela <luccaloyale@gmail.com>
2024-06-03 15:10:17 -0300

    Removendo Jsonignore desnecessario do usuario

--- atv3/automanager3/src/main/java/com/autobots/automanager/entidades/Usuario.java
+++ atv3/automanager3/src/main/java/com/autobots/automanager/entidades/Usuario.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToOne;
 import org.springframework.hateoas.RepresentationModel;
 
 import com.autobots.automanager.enumeracoes.PerfilUsuario;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -38,27 +37,19 @@ public class Usuario extends RepresentationModel<Usuario>{
 	@ElementCollection(fetch = FetchType.EAGER)
 	private Set<PerfilUsuario> perfis = new HashSet<>();
 	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-	@JsonIgnore
 	private Endereco endereco;
 	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JsonIgnore
 	private Set<Telefone> telefones = new HashSet<>();
 	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JsonIgnore
 	private Set<Documento> documentos = new HashSet<>();
 	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JsonIgnore
 	private Set<Email> emails = new HashSet<>();
 	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JsonIgnore
 	private Set<Credencial> credenciais = new HashSet<>();
 	@OneToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
-	@JsonIgnore
 	private Set<Mercadoria> mercadorias = new HashSet<>();
 	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
-	@JsonIgnore
 	private Set<Venda> vendas = new HashSet<>();
 	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
-	@JsonIgnore
 	private Set<Veiculo> veiculos = new HashSet<>();
 }
