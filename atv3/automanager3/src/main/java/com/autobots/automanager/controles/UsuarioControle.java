f6f37480dc6268a185ec6ecc12ce3b0d18e5859e
luccavilela <luccaloyale@gmail.com>
2024-06-04 10:10:04 -0300

    Arrumando deletar (cascata)

--- atv3/automanager3/src/main/java/com/autobots/automanager/controles/UsuarioControle.java
+++ atv3/automanager3/src/main/java/com/autobots/automanager/controles/UsuarioControle.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.autobots.automanager.entidades.Empresa;
 import com.autobots.automanager.entidades.Usuario;
 import com.autobots.automanager.modelos.atualizadores.UsuarioAtualizador;
 import com.autobots.automanager.modelos.links.AdicionadorLinkUsuario;
 import com.autobots.automanager.modelos.outros.UsuarioSelecionador;
+import com.autobots.automanager.repositorios.RepositorioEmpresa;
 import com.autobots.automanager.repositorios.UsuarioRepositorio;
 
 
@@ -26,6 +28,8 @@ import com.autobots.automanager.repositorios.UsuarioRepositorio;
 public class UsuarioControle {
 	@Autowired
 	private UsuarioRepositorio repositorio;
+	@Autowired
+	private RepositorioEmpresa empresaRepositorio;
 	@Autowired
 	private UsuarioSelecionador selecionador;
 	@Autowired
@@ -86,12 +90,17 @@ public class UsuarioControle {
 
 	@DeleteMapping("/usuario/excluir")
 	public ResponseEntity<?> excluirUsuario(@RequestBody Usuario exclusao) {
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		Usuario usuario = repositorio.getById(exclusao.getId());
-		if (usuario != null) {
-			repositorio.delete(usuario);
-			status = HttpStatus.OK;
-		}
-		return new ResponseEntity<>(status);
+	    HttpStatus status = HttpStatus.BAD_REQUEST;
+	    Usuario usuario = repositorio.getById(exclusao.getId());
+	    if (usuario != null) {
+	        List<Empresa> empresas = empresaRepositorio.findByUsuariosId(usuario.getId());
+	        for (Empresa empresa : empresas) {
+	            empresa.getUsuarios().remove(usuario);
+	            empresaRepositorio.save(empresa);
+	        }
+	        repositorio.delete(usuario);
+	        status = HttpStatus.OK;
+	    }
+	    return new ResponseEntity<>(status);
 	}
 }
--- atv3/automanager3/src/main/java/com/autobots/automanager/repositorios/RepositorioEmpresa.java
+++ atv3/automanager3/src/main/java/com/autobots/automanager/repositorios/RepositorioEmpresa.java
@@ -1,8 +1,11 @@
 package com.autobots.automanager.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.autobots.automanager.entidades.Empresa;
 
 public interface RepositorioEmpresa extends JpaRepository<Empresa, Long> {
+	List<Empresa> findByUsuariosId(Long usuarioId);
 }
